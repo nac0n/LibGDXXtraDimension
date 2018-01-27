8f68607b6a4b0695c5d8e32a567d183c5286221f
nac0n <lager_91@hotmail.com>
2018-01-27 12:06:05 +0100

    initial commit

---
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		config.title = "hello-world";
+		config.width = 1280;
+		config.height = 800;
+        
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
